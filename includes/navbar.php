3c682247bdf12fc6f586f5ba79c7db6f2b24fd49
Philipp Lackinger <Phillackinger@gmx.at>
2021-01-25 16:03:40 +0100

    ✨ Navbar
    + logo
    + navbar.php

---
+++ includes/navbar.php
@@ -0,0 +1,22 @@
+<?php
+echo "
+
+<!-- Navbar-Container -->
+
+<nav>
+    <div class='nav-wrapper deep-purple lighten-2'>
+            <a href='#'><img style='height: 100%' class='brand-logo' src='../content/graphics/logo.png' alt='Logo'></a> 
+
+            <ul id='nav-mobile' class='right hide-on-med-and-down'>
+                <li><a href='sass.html'><i class='material-icons black-text'>search</i></a></li>
+                <li><a href='badges.html'><i class='material-icons black-text'>view_module</i></a></li>
+                <li><a href='collapsible.html'><i class='material-icons black-text'>refresh</i></a></li>
+                <li><a href='mobile.html'><i class='material-icons black-text'>more_vert</i></a></li>            
+            </ul>    
+    </div>
+</nav>
+
+";
+
+
+
--- index.php
+++ index.php
@@ -1,2 +1,48 @@
 <?php
 
+include_once "includes/includes.php";
+
+
+?>
+
+<html lang="de">
+    <head>
+
+
+        <title>SEPVE - Control Panel</title>
+
+        <style>
+
+            .main{
+                padding: 2rem;
+                margin: 2rem;
+
+            }
+
+        </style>
+
+    </head>
+    <body>
+
+        <?php include_once "includes/navbar.php"; ?>
+
+
+        <div class="main">
+            <form action="#">
+                <p class="range-field">
+
+                    <label for="volume">Laustärke</label>
+                    <input type="range" id="volume" min="0" max="100" step="1" />
+                </p>
+            </form>
+        </div>
+
+
+
+    </body>
+
+</html>
+
+
+
+
